c1d3f0fd08cf73355769ccd508a99777124e8b73
jamie-edwards <edwards.jamie@outlook.com>
2020-02-05 14:11:02 +0000

    final gatsby site

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from "../components/layout"
+
+const AboutmePage = () => {
+    return (
+        <Layout>
+            <h1>About Me</h1>
+            <p>Currently working in London for small start up's and global companies.</p>
+            <p><Link to="/contact">Want to work with me? Get in touch.</Link></p>
+        </Layout>
+    )
+}
+
+export default AboutmePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <h1>Plan Code Grow</h1>
+            <h2>I'm Jamie, a full-stack developer living in London. Developing websites that build brands.</h2>
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+        </Layout>
+    )
+}
+
+export default IndexPage
