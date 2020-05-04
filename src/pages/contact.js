9fe7be0cf62f09f71d198245ff6a95b21d91b777
jamie-edwards <edwards.jamie@outlook.com>
2020-05-04 13:02:27 +0100

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,8 +6,7 @@ const ContactPage = () => {
     return (
         <Layout>
             <h1>Contact</h1>
-            <p>
-               Email studio@jamieedwards.co</p>
+            <p>Email studio@jamieedwards.co</p>
             <p>Twitter <a href="https://twitter.com/@jamie_edwards" target="_blank">@jamie_edwards</a>
             </p>
         </Layout>
