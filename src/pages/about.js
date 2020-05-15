d3a903f0502010c79effaed9316a627eda3b93f8
Jamie Edwards <43964089+jamie-edwards@users.noreply.github.com>
2020-05-15 13:32:58 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,8 @@ import Layout from "../components/layout"
 const AboutmePage = () => {
     return (
         <Layout>
-            <h1>About Me</h1>
-            <p>London based web solutions for small start up's and global companies.</p>
+            <h1>About</h1>
+            <p>I design and build bespoke projects. My background is in History of Art & Cultural studies read at The University of Leeds. Most recently, I was at Bonnier, before that I was at ComeOn! Group for over 3 years (2012–2017) during it’s start up phase. Over the past few years I have focused on front-end development projects across various verticals specialising in turning ideas into fully functional digital products..</p>
             <p><Link to="/contact">Want to work with me? Get in touch.</Link></p>
         </Layout>
     )
