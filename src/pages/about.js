e7b7878a39636184faa365b91cce4964fbd61a25
Jamie Edwards <43964089+jamie-edwards@users.noreply.github.com>
2020-05-15 13:37:29 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,8 @@ const AboutmePage = () => {
     return (
         <Layout>
             <h1>About</h1>
-            <p>I design and build bespoke projects. My background is in History of Art & Cultural studies read at The University of Leeds. Most recently, I was at Bonnier, before that I was at ComeOn! Group for over 3 years (2012–2017) during it’s start up phase. Over the past few years I have focused on front-end development projects across various verticals specialising in turning ideas into fully functional digital products..</p>
-            <p><Link to="/contact">Want to work with me? Get in touch.</Link></p>
+            <p>I design and build bespoke projects. My background is in History of Art & Cultural studies read at The University of Leeds. Most recently, I was at Bonnier, before that I was at ComeOn! Group for over 3 years (2012–2017) during it’s start up phase. Over the past few years I have focused on front-end development projects across various verticals. I specialise in turning ideas into fully functional digital products.</p>
+            <p><Link to="/contact">Get in touch.</Link></p>
         </Layout>
     )
 }
