d11badfad5bdb9207fd27a1aa94f60b98e39c066
Jamie Edwards <43964089+jamie-edwards@users.noreply.github.com>
2020-05-15 14:24:37 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,18 +8,10 @@ const ContactPage = () => {
             <h1>Contact</h1>
        
             <p>E: <a href="mailto:studio@jamieedwards.co">studio@jamieedwards.co</a></p>
+
+            <p><a href="https://www.instagram.com/edwards__jamie">Instagram</a> / <a href="https://www.linkedin.com/in/jamiesedwards">LinkedIn</a> / <p> <a href="https://twitter.com/@jamie_edwards" target="_blank">@jamie_edwards</a></p>
         
-<ul>
-<li>Academy Court</li>
-<li>Kirkwall Place</li>
-<li>London E2 0NQ</li>
-</ul>
-
-<p><a href="https://www.instagram.com/edwards__jamie">Instagram</a> / <a href="https://www.linkedin.com/in/jamiesedwards">LinkedIn</a> / <p> <a href="https://twitter.com/@jamie_edwards" target="_blank">@jamie_edwards</a></p>
-
-            </div>
             
-            <p>Twitter <a href="https://twitter.com/@jamie_edwards" target="_blank">@jamie_edwards</a>
         </Layout>
     )
 }
